5d780ec449df70c737a610b2cd32c370de758d3a
imjal/Users/jessicalee/anaconda3/envs/generalized-colorimetry/bin/python <jessicaannielee14@gmail.com>
2024-08-13 15:26:05 -0700

    encoding / decoding rgb to sequence seems to work.

---
+++ chromalab/led.py
@@ -0,0 +1,87 @@
+from tqdm import tqdm
+from enum import Enum
+from typing import List, Optional
+import numpy.typing as npt
+
+import numpy as np
+
+from .observer import Observer
+from .spectra import Spectra, Illuminant
+
+
+
+class LEDS(Enum):
+    RED = 0 # 660nm
+    GREEN = 1 # 550nm
+    BLUE = 2 # 451nm
+    YELLOW = 3 # 590nm
+    CYAN = 4 # 510nm
+    SBLUE = 5 # 410nm
+
+class LEDSequence:
+    HDMI_BANDWIDTH = 24
+
+    def __init__(self, leds: List[LEDS], sequence: List[LEDS], observer: Optional[Observer]=None):
+        self.primaries = leds
+        if len(leds) > 6: 
+            raise ValueError("Only up to 6 LEDs are supported")
+        elif len(leds) != len(set(leds)):
+            raise ValueError("LEDs Must Be Unique")
+        elif len(sequence) % 2 != 0 and len(sequence) % 3 != 0:
+            raise ValueError("Only supporting k that divides 24")
+        self.k = len(leds)
+        self.b = self.HDMI_BANDWIDTH // self.k
+
+        if len(sequence) != self.HDMI_BANDWIDTH:# check if 24 bits are used
+            raise ValueError("Sequence Must be 24 Bits")
+        elif not np.all(np.isin(sequence, self.primaries)):
+            raise ValueError("Sequence Must Only Contain LEDs in primary array")
+        
+        self.seq_idx = []
+        curr_leds = [0] * len(leds)
+        for i in range(24):
+            led_idx = sequence[i].value
+            self.seq_idx.append(curr_leds[led_idx])
+            curr_leds[led_idx]+=1
+        self.sequence = sequence
+        self.observer = observer
+
+    @staticmethod
+    def TetrachromaticDisplay():
+        return LEDSequence([LEDS.RED, LEDS.GREEN, LEDS.BLUE, LEDS.YELLOW], [LEDS.RED, LEDS.GREEN, LEDS.BLUE, LEDS.YELLOW] * 6)
+
+    # getters
+    def get_primaries(self):
+        return self.primaries
+
+    def get_sequence(self):
+        return self.sequence
+    
+    def encode_intensities_to_seq(self, intensities: npt.ArrayLike):
+        if len(intensities) != self.k:
+            raise ValueError("Intensities Need to Be Same Length as Primaries")
+        elif not np.logical_and(np.all(intensities >=0), np.all(intensities <= 1)):
+            raise ValueError("Intensities Need to be Between 0 and 1")
+
+        casted_intensities = np.array([intensities * 2 **self.b - 1], dtype=np.uint8)
+        casted_intensities = np.unpackbits(casted_intensities, axis=0).T
+        bin_seq = np.zeros(24, dtype=bool)
+        for i in range(24):
+            led_type = self.sequence[i].value
+            led_idx = self.seq_idx[i]
+            bin_seq[i] = casted_intensities[led_type][(8 - self.b) + led_idx]
+        bin_seq = bin_seq.reshape(3, 8)
+        encoded_rgb = np.packbits(bin_seq, axis=1).T.squeeze()
+        return encoded_rgb
+    
+    def decode_seq_to_intensities(self, encoded_rgb: npt.ArrayLike):
+        if len(encoded_rgb) != 3 or encoded_rgb.dtype != np.uint8:
+            raise ValueError("Encoded RGB Must be 3 Bytes")
+        casted_rgb = np.unpackbits(encoded_rgb)
+        intensities = np.zeros((self.k, 8), dtype=bool)
+        for i in range(24):
+            led_type = self.sequence[i].value
+            led_idx = self.seq_idx[i]
+            intensities[led_type][(8 - self.b) + led_idx] = casted_rgb[i]
+        intensities = np.packbits(intensities, axis=1).T
+        return intensities/(2 **self.b - 1)
